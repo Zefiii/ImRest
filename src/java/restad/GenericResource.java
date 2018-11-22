f0487168c531410b553fe251545a3ca925f73021
jrubiralta <jordiruby@gmail.com>
2018-11-22 21:19:32 +0100

    Modificar fet. Falta acabar cerques i esborrat

--- src/java/restad/GenericResource.java
+++ src/java/restad/GenericResource.java
@@ -149,8 +149,7 @@ public class GenericResource {
             @FormParam ("title") String title,
             @FormParam ("description") String description,
             @FormParam ("keywords") String keywords,
-            @FormParam ("author") String author,
-            @FormParam("creation") String creation){
+            @FormParam ("author") String author){
         
         String html = htmini;
         
@@ -177,8 +176,7 @@ public class GenericResource {
                    + "<p>Titol: " + title + "</p></br>"
                    + "<p>Autor: " + author + "</p></br>"
                    + "<p>" + description + "</p></br>"
-                   + "<p>Keywords: " + keywords + "</p></br>"
-                   + "<p>Data de creacio: " + creation + "</p>";
+                   + "<p>Keywords: " + keywords + "</p></br>";
         }
         catch(SQLException e){
             System.out.println(e);
@@ -193,7 +191,7 @@ public class GenericResource {
                 System.err.println(e.getMessage());
             }
         }
-        html = html + htmfini;
+        html = html + "<a href=\"../../index.html\" id=\"refToMenu\">Tornar al menu</a>"+htmfini;
         return html;
     }
     
@@ -298,15 +296,19 @@ public class GenericResource {
                path_imatge = "../../imatges/" + titol + id_imatge + ".jpeg";
                html = html + "<div><h2>" + titol +"</h2>"
                        + "<img src=\"" + path_imatge + "\" alt=" + titol + ">"
-                       +"<p>Descripcio: " + descripcio +"</p>"
-                       +"<p>Autor: " + autor +"</p>"
-                       +"<p>Data de creacio: " + creacio +"</p></div></br></br>";
+                       + "<p>Descripcio: " + descripcio +"</p>"
+                       + "<p>Autor: " + autor +"</p>"
+                       + "<p>Data de creacio: " + creacio +"</p></div>"
+                       + "<form action=\"../../modificarImatge.jsp\" method=\"post\">"
+                       + "<input type=\"hidden\" value=\"" + id_imatge + "\" name=\"id_imatge\" id=\"id_imatge\">"
+                       + "<input type=\"submit\" value=\"Modificar imatge\"></form>";
            }
-           html = html + htmfini;
+        html = html + "<a href=\"../../index.html\" id=\"refToMenu\">Tornar al menu</a>"+htmfini;
                       
         } catch(SQLException e){
               System.err.println(e.getMessage());
-              html = "<html><head/><body><h1>Llistar Incorrecte :(!</h1></body></html>";
+              html = "<html><head/><body><h1>Llistar Incorrecte :(!</h1></body></html>"
+                      + "<a href=\"../../index.html\" id=\"refToMenu\">Tornar al menu</a>";
 
         } 
         finally{
@@ -337,25 +339,32 @@ public class GenericResource {
             System.out.println("Error class.forname");
         }
         try{
+            System.out.println(id);
             //conn = DriverManager.getConnection("jdbc:sqlite:F:\\windows\\ADPractica4\\loquesea.db"); 
             //conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Oriol\\Desktop\\basedades.db");
             //conn = DriverManager.getConnection("jdbc:sqlite:\\Users\\oriol\\OneDrive\\Escritorio\\loquesea.db");
             conn = DriverManager.getConnection("jdbc:sqlite:/Users/Jordi/Desktop/loquesea.db");
-            PreparedStatement statement =  conn.prepareStatement("select * from imagenes where id_imagen = ?");
-            statement.setString(1, String.valueOf(id));
+            PreparedStatement statement =  conn.prepareStatement("select * from imagenes where id_imagen = ?"); 
+            statement.setInt(1, id);
             ResultSet rs = statement.executeQuery();
-            html = html + "<div>";
-            html = html + "<h2>Titol: " + rs.getString("titulo") + "</h2><br>";
-            html = html + "<p>Autor: " + rs.getString("autor") + "<p><br>";
-            html = html + "<p>" + rs.getString("descripcion") + "</p> <br>";
-            html = html + "<p>Paraules clau: " + rs.getString("palabras_clave") +  "</p><br>";
-            html = html + "<p>Data de creacio: " + rs.getString("creacion") + "</p><br>";
-            html = html + "</div>";
-            html = html + htmfini;
+            if(rs.next()){
+                html = html + "<div>"
+                        + "<h2>Titol: " + rs.getString("titulo") + "</h2><br>"
+                        + "<p>Autor: " + rs.getString("autor") + "<p><br>"
+                        + "<p>" + rs.getString("descripcion") + "</p> <br>"
+                        + "<p>Paraules clau: " + rs.getString("palabras_clave") +  "</p><br>"
+                        + "<p>Data de creacio: " + rs.getString("creacion") + "</p><br>"
+                        + "</div>"
+                        + "<form action=\"../../modificarImatge.jsp\" method=\"post\">"
+                        + "<input type=\"hidden\" value=\"" + id + "\" name=\"id_imatge\" id=\"id_imatge\">"
+                        + "<input type=\"submit\" value=\"Modificar imatge\"></form>"
+                        + "<a href=\"../../../index.html\" id=\"refToMenu\">Tornar al menu</a>" + htmfini;
+            }
         }
         catch(SQLException e){
             System.out.println(e);
-            html = "<html></header><body><h1> No hi ha cap Imatge amb aquest ID o hi ha hagut un error</h1></body></html>";
+            html = "<html></header><body><h1> No hi ha cap Imatge amb aquest ID o hi ha hagut un error</h1>"
+                    + "<a href=\"../../../index.html\" id=\"refToMenu\">Tornar al menu</a></body></html>";
         }
         finally{
             try{
@@ -390,33 +399,39 @@ public class GenericResource {
             conn = DriverManager.getConnection("jdbc:sqlite:/Users/Jordi/Desktop/loquesea.db");
             PreparedStatement statement =  conn.prepareStatement("select * from imagenes where titulo = ?"); 
             statement.setString(1, title);
-            
-            
             ResultSet rs = statement.executeQuery();
             if(rs.next()){
-                html = html + "<div>";
-                    html = html + "<h2>Titol: " + rs.getString("titulo") + "</h2><br>";
-                    html = html + "<p>Autor: " + rs.getString("autor") + "<p><br>";
-                    html = html + "<p>" + rs.getString("descripcion") + "</p> <br>";
-                    html = html + "<p>Paraules clau: " + rs.getString("palabras_clave") +  "</p><br>";
-                    html = html + "<p>Data de creacio: " + rs.getString("creacion") + "</p><br>";
-                    html = html + "</div>";
+                html = html + "<div>"
+                        + "<h2>Titol: " + rs.getString("titulo") + "</h2><br>"
+                        + "<p>Autor: " + rs.getString("autor") + "<p><br>"
+                        + "<p>" + rs.getString("descripcion") + "</p> <br>"
+                        + "<p>Paraules clau: " + rs.getString("palabras_clave") +  "</p><br>"
+                        + "<p>Data de creacio: " + rs.getString("creacion") + "</p><br>"
+                        + "</div>"
+                        + "<form action=\"../../../modificarImatge.jsp\" method=\"post\">"
+                        + "<input type=\"hidden\" value=\"" + rs.getString("id_imagen") + "\" name=\"id_imatge\" id=\"id_imatge\">"
+                        + "<input type=\"submit\" value=\"Modificar imatge\"></form>";
                 while (rs.next()) {                
-                    html = html + "<div>";
-                    html = html + "<h2>Titol: " + rs.getString("titulo") + "</h2><br>";
-                    html = html + "<p>Autor: " + rs.getString("autor") + "<p><br>";
-                    html = html + "<p>" + rs.getString("descripcion") + "</p> <br>";
-                    html = html + "<p>Paraules clau: " + rs.getString("palabras_clave") +  "</p><br>";
-                    html = html + "<p>Data de creacio: " + rs.getString("creacion") + "</p><br>";
-                    html = html + "</div>";
+                    html = html + "<div>"
+                        + "<h2>Titol: " + rs.getString("titulo") + "</h2><br>"
+                        + "<p>Autor: " + rs.getString("autor") + "<p><br>"
+                        + "<p>" + rs.getString("descripcion") + "</p> <br>"
+                        + "<p>Paraules clau: " + rs.getString("palabras_clave") +  "</p><br>"
+                        + "<p>Data de creacio: " + rs.getString("creacion") + "</p><br>"
+                        + "</div>"
+                        + "<form action=\"../../../modificarImatge.jsp\" method=\"post\">"
+                        + "<input type=\"hidden\" value=\"" + rs.getString("id_imagen") + "\" name=\"id_imatge\" id=\"id_imatge\">"
+                        + "<input type=\"submit\" value=\"Modificar imatge\"></form>";
                 }
-                html = html + htmfini; 
+                html = html + "<a href=\"../../../index.html\" id=\"refToMenu\">Tornar al menu</a>" + htmfini; 
             }
-            else return "<html></header><body><h1> No hi ha cap Imatge amb aquest titol o hi ha hagut un error</h1></body></html>";
+            else html = "<html></header><body><h1> No hi ha cap Imatge amb aquest titol o hi ha hagut un error</h1>"
+                    + "<a href=\"../../../index.html\" id=\"refToMenu\">Tornar al menu</a></body></html>";
       
         } catch(SQLException e) {
             System.out.println(e);
-           return "<html></header><body><h1> No hi ha cap Imatge amb aquest titol o hi ha hagut un error</h1></body></html>";
+            html = "<html></header><body><h1> No hi ha cap Imatge amb aquest titol o hi ha hagut un error</h1>"
+                    + "<a href=\"../../../index.html\" id=\"refToMenu\">Tornar al menu</a></body></html>";
         }
         finally{
             try{
@@ -471,12 +486,14 @@ public class GenericResource {
                     html = html + "<p>Data de creacio: " + rs.getString("creacion") + "</p><br>";
                     html = html + "</div>";
                 }
-                html = html + htmfini; 
+                html = html + "<a href=\"../../index.html\" id=\"refToMenu\">Tornar al menu</a>" + htmfini; 
             }
-            else html = "<html></header><body><h1> No hi ha cap Imatge amb aquesta data de creacio o hi ha hagut un error</h1></body></html>";
+            else html = "<html></header><body><h1> No hi ha cap Imatge amb aquesta data de creacio o hi ha hagut un error</h1></body></html>"
+                    + "<a href=\"../../index.html\" id=\"refToMenu\">Tornar al menu</a>";
         } catch(SQLException e) {
             System.out.println(e);
-            html = "<html></header><body><h1> No hi ha cap Imatge amb aquesta data de creacio o hi ha hagut un error</h1></body></html>";
+            html = "<html></header><body><h1> No hi ha cap Imatge amb aquesta data de creacio o hi ha hagut un error</h1>"
+                    + "<a href=\"../../index.html\" id=\"refToMenu\">Tornar al menu</a></body></html>";
         }  
         finally{
             try{
@@ -531,12 +548,14 @@ public class GenericResource {
                     html = html + "<p>Data de creacio: " + rs.getString("creacion") + "</p><br>";
                     html = html + "</div>";
                 }
-                html = html + htmfini; 
+                html = html + "<a href=\"../../index.html\" id=\"refToMenu\">Tornar al menu</a>" + htmfini; 
             }
-            else html = "<html></header><body><h1> No hi ha cap Imatge amb aquest autor o hi ha hagut un error</h1></body></html>";
+            else html = "<html></header><body><h1> No hi ha cap Imatge amb aquest autor o hi ha hagut un error</h1></body></html>"
+                    + "<a href=\"../../index.html\" id=\"refToMenu\">Tornar al menu</a>";
         } catch(SQLException e) {
             System.out.println(e);
-            html = "<html></header><body><h1> No hi ha cap Imatge amb aquest autor o hi ha hagut un error</h1></body></html>";
+            html = "<html></header><body><h1> No hi ha cap Imatge amb aquest autor o hi ha hagut un error</h1>"
+                    + "<a href=\"../../index.html\" id=\"refToMenu\">Tornar al menu</a></body></html>";
         }
         finally{
             try{
@@ -597,12 +616,14 @@ public class GenericResource {
                         html = html + "</div>";
                     } 
                 }
-                html = html + htmfini; 
+                html = html + "<a href=\"../../index.html\" id=\"refToMenu\">Tornar al menu</a>" + htmfini; 
             }
-            else if(!trobat) html = "<html></header><body><h1> No hi ha cap Imatge amb aquestes paraules clau o hi ha hagut un error</h1></body></html>";
+            else if(!trobat) html = "<html></header><body><h1> No hi ha cap Imatge amb aquestes paraules clau o hi ha hagut un error</h1></body></html>"
+                    + "<a href=\"../../index.html\" id=\"refToMenu\">Tornar al menu</a>";
         } catch(SQLException e) {
             System.out.println(e);
-            html = "<html></header><body><h1> No hi ha cap Imatge amb aquestes paraules clau o hi ha hagut un error</h1></body></html>";
+            html = "<html></header><body><h1> No hi ha cap Imatge amb aquestes paraules clau o hi ha hagut un error</h1>"
+                    + "<a href=\"../../../index.html\" id=\"refToMenu\">Tornar al menu</a></body></html>";
         }  
         finally{
             try{
--- web/modificarImatge.jsp
+++ web/modificarImatge.jsp
@@ -4,38 +4,93 @@
     Author     : oriol.nin
 --%>
 
+<%@page import="java.io.PrintWriter"%>
+<%@page import="java.sql.SQLException"%>
+<%@page import="java.sql.ResultSet"%>
+<%@page import="java.sql.PreparedStatement"%>
+<%@page import="java.sql.DriverManager"%>
+<%@page import="java.sql.Connection"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
      <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Registrar Imatge</title>
+        <title>Modificar Imatge</title>
     </head>
     <body>
         <div>
             <h1 id="headerModicarImatge">Modificar Imatge</h1>
             <p id="dades"><strong>Introdueix les dades que es demanen a continuació</strong></p>
-            <form method="post" action="webresources/generic/modify" id="modificarImatge">
-                <label for="id"><br>ID:</br></label>
-                <input type="text" name="id" id="id">
-                <br>
-                <label for="title"><br>Títol:<br></label>
-                <input type="text" name="title" id="title">
-                <br>
-                <textarea cols='30' rows='10' name="description" id="description" placeholder="Escriu una breu descripció..."></textarea>
-                <br>
-                <label for="keywords"><br>Paraules clau:<br></label>
-                <input type="text" name="keywords" id="keywords">
-                <br>
-                <label for="author"><br>Autor:<br></label>
-                <input type="text" name="author" id="author">
-                <br>
-                <label for="creation"><br>Data de creació:<br></label>
-                <input type="date" name="creation" id="creation">
-                <br>
-                <input type="submit" name="modificar" id="registrar" value="Modificar">
-                <input type="reset" name="netejar" id="netejarQuestionari" value="Esborrar dades">
-            </form>
+            <%
+                Connection conn = null;
+                String id_modificar = request.getParameter("id_imatge");
+                Class.forName("org.sqlite.JDBC");
+                String user = (String) session.getAttribute("user");
+
+                try{
+                    //conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Oriol\\Desktop\\basedades.db");
+                    conn = DriverManager.getConnection("jdbc:sqlite:/Users/Jordi/Desktop/loquesea.db");
+                    //conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\oriol\\OneDrive\\Escritorio\\loquesea.db");
+                    System.out.print(id_modificar);
+                    PreparedStatement statement =  conn.prepareStatement("select * from imagenes where id_imagen = ?");
+                    statement.setString(1, id_modificar);
+                    
+                    String titol;
+                    String descripcio;
+                    String clau;
+                    String autor;
+                    String path;
+                    
+                    ResultSet rs = statement.executeQuery();
+
+                    if (rs.next()) {
+                        titol = rs.getString("titulo");
+                        descripcio = rs.getString("descripcion");
+                        clau = rs.getString("palabras_clave");
+                        autor = rs.getString("autor");
+                        path = "imatges/" + titol +  id_modificar + ".jpeg";
+                        
+                        out.println("<br><img src=\"" + path + "\"><br><br>");
+                        
+                        out.println("<form method=\"post\" action=\"webresources/generic/modify\" id=\"modificarImatge\">");
+                        out.println("<label for=\"title\">Títol: " + titol + "</label>");
+                        out.println("<input type=\"hidden\" value=\"" + titol + "\" name=\"title\" id=\"title\"><br>");
+                                                
+                        out.println("<label for=\"description\">Descripció: </label><br>");
+                        out.println("<textarea cols='30' rows='10' name=\"description\" id=\"description\">" + descripcio + "</textarea><br>");
+                       
+                        
+                        out.println("<label for=\"author\">Autor: </label>");
+                        out.println("<input type=\"text\" name=\"author\" id=\"author\" value=\"" + autor + "\"><br>");
+
+                        out.println("<label for=\"keywords\">Paraules Clau: </label>");
+                        out.println("<input type=\"text\" name=\"keywords\" id=\"keywords\" value=\"" + clau + "\"><br>"
+                                + "<input type=\"hidden\" value=\"" + id_modificar + "\" name=\"id\" id=\"id\">");
+                        
+                        
+                        
+                        out.println("<input type=\"submit\" id=\"modificar\" value=\"Modificar\">");
+                        out.println("</form>");
+                    }
+                }
+                catch(SQLException e)
+                {
+                  System.err.println(e.getMessage());
+                }   
+                finally{
+                   try
+                  {
+                    if(conn != null)
+                      conn.close();
+                  }
+                  catch(SQLException e)
+                  {
+                    // connection close failed.
+                    System.err.println(e.getMessage());
+                  }
+                }
+            %>
+                
         </div>
     </body>
 </html>
